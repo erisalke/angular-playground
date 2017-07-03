2cb2e7f33c0437452233112adb44941816627436
piotrek <erisalke@gmail.com>
2017-07-03 15:51:49 +0200

    two way date binding example

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-servers]',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent implements OnInit {
 
-  constructor() { }
+export class ServersComponent implements OnInit {
+  serverName = '';
+  description = 'Nothing';
+  isDisabled = true;
+
+  constructor() { 
+    setTimeout(()=> this.isDisabled=false, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onClick() {
+    this.description = 'A change happened!';
+  }
+
+  onUpdateServerName (event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
