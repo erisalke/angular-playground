026dbab44428a113e12a2238205f86734314d37e
piotrek <erisalke@gmail.com>
2017-07-04 16:45:03 +0200

    add recipe list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
   ],
   imports: [
     BrowserModule,
