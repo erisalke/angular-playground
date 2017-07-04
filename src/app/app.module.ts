ef59c312e749c6973a9a167a06c966e23a777def
piotrek <erisalke@gmail.com>
2017-07-04 16:59:37 +0200

    add recipe detail view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
-
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
@@ -14,7 +13,6 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
   declarations: [
     AppComponent,
     NavbarComponent,
-
     RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
